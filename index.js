392a147fc6ac94ee651c0afb07b5d73af95bfa26
Amitash <amitu895@gmail.com>
2022-08-09 23:25:36 +0530

    Draws circles without lines connecting it and added CSS

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ var state = 0;
 // 0 is draw circle
 // 1 is type text
 // 2 is draw line
+var btns = [document.getElementById("draw"), document.getElementById("text"), document.getElementById("line1"), document.getElementById("line2")];
 
-document.getElementById("draw").addEventListener("click", ()=>{state = 0;});
-document.getElementById("text").addEventListener("click", ()=>{state = 1;});
-document.getElementById("line1").addEventListener("click", ()=>{state = 2;});
-document.getElementById("line2").addEventListener("click", ()=>{state = 3;});
+for(let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", ()=>{state = i;});
+}
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var ctx = canvas.getContext("2d");
 ctx.fillStyle="cyan";
-ctx.font = "25px Arial";
+ctx.font = "22px Arial";
 
 // ctx.arc(200,150,100,0,360);
 // ctx.arc(400,150,50,0,360);
@@ -27,17 +27,20 @@ function printMousePos(event) {
   // ctx.stroke();
   ctx.fillStyle = "cyan";
   switch(state) {
-    case 0: 
-      ctx.fillRect(event.clientX - 25, event.clientY - 50, 50, 50);
-      // ctx.arc(event.clientX, event.clientY, 20, 0, 360); ctx.stroke();
+    case 0:
+      ctx.beginPath(); //starts a new line
+      // ctx.fillRect(event.clientX - 25, event.clientY - 50, 50, 50);
+      ctx.arc(event.clientX, event.clientY - 25, 25, 0, 360); ctx.stroke();
       break;
     case 1:
       ctx.fillStyle = "black";
-      ctx.fillText(document.getElementById("text_").value, event.clientX - 5, event.clientY - 12);
+      ctx.fillText(document.getElementById("text_value").value, event.clientX - 5, event.clientY - 12);
+      break;
     case 2:
-      ctx.moveTo(event.clientX, event.clientY - 20); break;
+      ctx.beginPath();
+      ctx.moveTo(event.clientX, event.clientY - 20); state = 3;break;
     case 3:
-      ctx.lineTo(event.clientX, event.clientY - 20); ctx.stroke(); break;
+      ctx.lineTo(event.clientX, event.clientY - 20); ctx.stroke(); state = 2; break;
   }
 }
 
