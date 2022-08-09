41c45e9bdbe5fc6825ef6298274bac4dcf17c0b2
Amitash <amitu895@gmail.com>
2022-08-10 00:04:10 +0530

    Checking if user clicks on circle

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var canvas= document.getElementById("myCanvas");
 var btns = [document.getElementById("draw"), document.getElementById("text"), document.getElementById("line1"), document.getElementById("line2")];
+
+let circles = []; radius = 25;
+
 function checkState(x) {
   for(let i = 0; i < btns.length; i++) {
     if(i === x)
@@ -26,6 +29,10 @@ var ctx = canvas.getContext("2d");
 ctx.fillStyle="cyan";
 ctx.font = "22px Arial";
 
+function isIntersect(point, circle) {
+  return Math.sqrt((point.x-circle.x) ** 2 + (point.y - circle.y) ** 2) < radius;
+}
+
 // ctx.arc(200,150,100,0,360);
 // ctx.arc(400,150,50,0,360);
 
@@ -33,23 +40,33 @@ function printMousePos(event) {
   // console.log("clientX: " + event.clientX + " - clientY: " + event.clientY);
   // ctx.arc(event.clientX, event.clientY, 20, 0, 360);
   // ctx.stroke();
+  const pos = {
+    x: event.clientX,
+    y: event.clientY
+  };
+  circles.forEach(circle => {
+    if (isIntersect(pos, circle)) {
+      console.log('yay')
+    }
+  });
 
   ctx.fillStyle = "cyan";
   switch(state) {
     case 0:
       ctx.beginPath(); //starts a new line
       // ctx.fillRect(event.clientX - 25, event.clientY - 50, 50, 50);
-      ctx.arc(event.clientX, event.clientY - 25, 25, 0, 2 * Math.PI); ctx.stroke();
+      circles.push({x:event.clientX, y:event.clientY});
+      ctx.arc(event.clientX, event.clientY - radius, radius, 0, 2 * Math.PI); ctx.fill();
       break;
     case 1:
       ctx.fillStyle = "black";
-      ctx.fillText(document.getElementById("text_value").value, event.clientX - 5, event.clientY - 12);
+      ctx.fillText(document.getElementById("text_value").value, event.clientX - 5, event.clientY - 22);
       break;
     case 2:
       ctx.beginPath();
-      ctx.moveTo(event.clientX, event.clientY - 20); state = 3; break;
+      ctx.moveTo(event.clientX, event.clientY - 20); state = 3; checkState(state); break;
     case 3:
-      ctx.lineTo(event.clientX, event.clientY - 20); ctx.stroke(); state = 2; break;
+      ctx.lineTo(event.clientX, event.clientY - 20); ctx.stroke(); state = 2; checkState(state); break;
   }
 }
 
